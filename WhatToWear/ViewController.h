fe52abf7e68a8e41e46824c989b52dc8ebdc931f
Alyza Malunao <amalu001@ucr.edu>
2014-12-06 14:02:39 -0800

    Initial Commit

---
+++ WhatToWear/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatToWear
+//
+//  Created by Alyza Malunao on 12/6/14.
+//  Copyright (c) 2014 WTW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
